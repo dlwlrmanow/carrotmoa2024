799bbdd8edd6b39077a7fb73c45d53857cca4598
Yijae <lullulalla9711@gmail.com>
2024-12-03 13:16:26 +0900

    edit: 반환타입 ResponseEntity로 변경

--- src/main/java/carrotmoa/carrotmoa/controller/api/AccommodationKeywordSearchResultApiController.java
+++ src/main/java/carrotmoa/carrotmoa/controller/api/AccommodationKeywordSearchResultApiController.java
@@ -3,15 +3,17 @@ package carrotmoa.carrotmoa.controller.api;
 import carrotmoa.carrotmoa.model.response.AccommodationResultResponse;
 import carrotmoa.carrotmoa.service.AccommodationSearchResultService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/guest")
+@RequestMapping("/api/guest/search")
 public class AccommodationKeywordSearchResultApiController {
     private final AccommodationSearchResultService accommodationSearchResultService;
 
@@ -19,9 +21,14 @@ public class AccommodationKeywordSearchResultApiController {
     public AccommodationKeywordSearchResultApiController(AccommodationSearchResultService accommodationSearchResultService) {
         this.accommodationSearchResultService = accommodationSearchResultService;
     }
-    @GetMapping("/search")
-    public List<AccommodationResultResponse> searchByKeyword(@RequestParam("keyword") String keyword) {
+
+    @GetMapping
+    public ResponseEntity<List<AccommodationResultResponse>> searchByKeyword(@RequestParam("keyword") String keyword) {
         List<AccommodationResultResponse> rooms = accommodationSearchResultService.searchAccommodations(keyword);
-        return rooms;
+
+        if(rooms.isEmpty() || rooms == null){
+            return ResponseEntity.ok(Collections.emptyList());
+        }
+        return ResponseEntity.ok(rooms);
     }
 }
