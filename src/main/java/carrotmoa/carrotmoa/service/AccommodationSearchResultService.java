bd380ea98dd32ce2c2e18cb758438f37c1050da8
olzh <99233019+dlwlrmanow@users.noreply.github.com>
2024-12-14 15:28:32 +0900

    edit: keyword 검색 무한스크롤 및 레디스 적용 (#7)

--- src/main/java/carrotmoa/carrotmoa/service/AccommodationSearchResultService.java
+++ src/main/java/carrotmoa/carrotmoa/service/AccommodationSearchResultService.java
@@ -25,21 +25,22 @@ public class AccommodationSearchResultService {
 
     @Transactional(readOnly = true)
     public List<AccommodationResultResponse> searchAccommodations(String keyword, Long lastId, int limit) {
-        // redis에 데이터가 있나 확인
-        String redisKey = "accommodations::" + keyword + "::" + lastId;
-        List<AccommodationResultResponse> cachedRedistData = (List<AccommodationResultResponse>) redisTemplate.opsForValue().get(redisKey);
-
-        if(cachedRedistData != null) {
-            // redis에 데이터 있으면 있는 거 주기
-            return cachedRedistData;
+        // 1. redis에 데이터가 있는지 먼저 확인하기
+        String redisKey = "accommodations::" + keyword + "::" + lastId + "::" + limit;
+        List<AccommodationResultResponse> cachedRedisData = (List<AccommodationResultResponse>) redisTemplate.opsForValue().get(redisKey);
+        if(cachedRedisData != null) {
+            System.out.println("redis if문 탐");
+            return cachedRedisData;
         }
 
+        // 데이터가 존재하지 않는 처음의 경우 디비에서 가져와서 뿌리기
         List<Object[]> results = accommodationRepository.searchAccommodationByKeyword(keyword, lastId, limit);
         List<AccommodationResultResponse> accommodations = results.stream()
                         .map(AccommodationResultResponse::fromData)
                         .collect(Collectors.toList());
 
-        redisTemplate.opsForValue().set(redisKey, accommodations, Duration.ofMinutes(30));
+        redisTemplate.opsForValue().set(redisKey, accommodations, Duration.ofMinutes(30)); // TTL로 30분 설정
+
 //        return results.stream()
 //            .map(AccommodationResultResponse::fromData)
 //            .collect(Collectors.toList());
