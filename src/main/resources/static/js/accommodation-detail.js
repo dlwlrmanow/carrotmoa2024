9384b581878ce7fa535af60d46b7af76a33c907d
Yijae <lullulalla9711@gmail.com>
2024-12-25 15:05:14 +0900

    edit: api분리에 따른 html 수정

--- src/main/resources/static/js/accommodation-detail.js
+++ src/main/resources/static/js/accommodation-detail.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const roomDetailContainer = document.getElementById("room-detail-info");
+
+    roomDetailContainer.addEventListener("click", function (event) {
+        const card = event
+    })
+
     const roomId = document.querySelector('input[name="id"]').value;
     const url = `/api/guest/room/detail?id=${roomId}`;
     console.log(roomId);
@@ -12,20 +18,21 @@ document.addEventListener("DOMContentLoaded", function () {
             .catch(error => {
                 displayErrorMessage();
             })
-        function renderRoomDetail(data) {
-            const roomDetailContainer = document.getElementById("room-detail-info");
+    }
 
-            roomDetailContainer.innerHTML = '';
+    function renderRoomDetail(data) {
+        // const roomDetailContainer = document.getElementById("room-detail-info");
+        roomDetailContainer.innerHTML = '';
 
-            data.forEach(data => {
+        data.forEach(data => {
+            const roomInfo = roomDetailContainer.content.cloneNode(true);
 
-            })
+            const
+        })
 
-            })
-        }
+    }
 
-        function displayErrorMessage(){
-            alert("해당 숙소를 이용할 수 없습니다.");
-        }
+    function displayErrorMessage(){
+        alert("해당 숙소를 이용할 수 없습니다.");
     }
 })
